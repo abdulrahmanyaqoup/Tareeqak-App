403389085a83c3de5c64ba04c2c82ac98560cfa6
ibramaswadeh <ibrahim.masswadeh@gmail.com>
2024-06-30 14:16:57 +0300

    add persistent user data

---
+++ server/middleware/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) {
+      return res.status(401).send({ error: "No auth token, access denied" });
+    }
+
+    const verified = jwt.verify(token, "passwordKey");
+
+    if (!verified) {
+      return res
+        .status(401)
+        .send({ error: "Token verification failed, authorization denied" });
+    }
+
+    req.user = verified.id;
+    req.token = token;
+    next();
+  } catch (error) {
+    res.status(500).send({ error: "error.message" });
+  }
+};
+
+module.exports = auth;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const auth = require("../middleware/auth");
 const authRouter = express.Router();
 
 // Sign Up
@@ -15,7 +16,9 @@ authRouter.post("/api/signup", async (req, res) => {
 
     const userExist = await User.findOne({ email });
     if (userExist) {
-      return res.status(400).json({ error: "User already exists" });
+      return res
+        .status(400)
+        .json({ error: "User with the same email already exists!" });
     }
 
     const hashedPassword = await bcrypt.hash(password, 8);
@@ -50,11 +53,39 @@ authRouter.post("/api/signin", async (req, res) => {
 
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
-      return res.status(400).json({ error: "email or password is invalid!" });
+      return res.status(400).json({ error: "password is not correct!" });
     }
 
     const token = jwt.sign({ _id: user._id }, "passwordKey");
     res.json({ token, ...user._doc });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// token verification
+authRouter.post("/tokenIsValid", auth, async (req, res) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) return res.json(false);
+
+    const verified = jwt.verify(token, "passwordKey");
+    if (!verified) return res.json(false);
+
+    const user = await User.findById(verified._id);
+    if (!user) return res.json(false);
+
+    res.json(true);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// get user data
+authRouter.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    res.json({ ...user._doc, token: req.token });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
