8d141ecaa903aa79f104067541fa5667f52929b9
ibramaswadeh <ibrahim.masswadeh@gmail.com>
2024-06-30 19:47:30 +0300

    implement update user api

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -26,9 +26,8 @@ authRouter.post("/api/signup", upload.single("image"), async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 8);
 
     const image = req.file.buffer;
-    const mimeType = req.file.mimetype; // Accessing the MIME type of the uploaded file
+    const mimeType = req.file.mimetype;
 
-    // Check if the MIME type is not one of the allowed types
     if (
       !["image/jpeg", "image/jpg", "image/png", "image/webp"].includes(mimeType)
     ) {
@@ -119,6 +118,72 @@ authRouter.get("/api/users", async (req, res) => {
   try {
     const users = await User.find({}, "name userProps");
     res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Update user
+authRouter.patch("/api/user/:id", auth, async (req, res) => {
+  try {
+    if (req.user._id !== req.params.id) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+
+    const updates = { ...req.body };
+    if (updates.userProps && Object.keys(updates.userProps).length === 0) {
+      delete updates.userProps;
+    }
+
+    const userExist = await User.findOne({ email: updates.email });
+    if (userExist) {
+      return res
+        .status(400)
+        .json({ error: "User with the same email already exists!" });
+    }
+
+    if (updates.name) {
+      if (updates.name.length === 0) {
+        return res.status(401).json({ error: "name can't be empty" });
+      }
+    }
+
+    if (updates.password) {
+      if (updates.password.length > 0) {
+        updates.password = await bcrypt.hash(updates.password, 8);
+      } else {
+        return res.status(401).json({ error: "password can't be empty" });
+      }
+    }
+
+    if (req.file) {
+      const mimeType = req.file.mimetype;
+      if (
+        !["image/jpeg", "image/jpg", "image/png", "image/webp"].includes(
+          mimeType
+        )
+      ) {
+        return res.status(422).json({
+          error:
+            "Unsupported image format. Allowed formats are: jpg, jpeg, png, webp.",
+        });
+      }
+
+      updates.userProps.image = req.file.buffer;
+    }
+
+    const user = await User.findByIdAndUpdate(
+      req.params.id,
+      updates,
+      { $set: updates },
+      { new: true }
+    );
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(user);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
