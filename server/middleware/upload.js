5ace63093ba66f8b4d3570f64032884abd56758d
ibramaswadeh <ibrahim.masswadeh@gmail.com>
2024-07-05 21:12:03 +0300

    fixes

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -1,6 +1,15 @@
 const multer = require("multer");
+const path = require("path");
 
-const storage = multer.memoryStorage();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); // Directory where images will be stored
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname); // Unique filename
+  },
+});
+
 const upload = multer({ storage: storage });
 
 module.exports = upload;
