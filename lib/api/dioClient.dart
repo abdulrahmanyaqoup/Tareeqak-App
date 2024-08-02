d58105a3fe1534c617967a5a2864424d14a52d4e
ibramaswadeh <ibrahim.masswadeh@gmail.com>
2024-08-02 19:35:03 +0300

    fix http

--- lib/api/dioClient.dart
+++ lib/api/dioClient.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
-import 'package:dio_http2_adapter/dio_http2_adapter.dart';
+import 'package:dio/io.dart';
 import 'package:finalproject/api/dioExceptionHandler.dart';
 import 'package:finalproject/env/env.dart';
 
@@ -10,17 +12,20 @@ Dio createDio() {
     responseType: ResponseType.json,
     headers: {
       'x-api-key': Env.API_KEY,
-      'content-Type': 'application/json; charset=UTF-8',
-      'accept': 'application/json',
+      HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+      HttpHeaders.acceptHeader: 'application/json',
     },
+    connectTimeout: const Duration(seconds: 5),
+    receiveTimeout: const Duration(seconds: 3),
+    sendTimeout: const Duration(seconds: 3),
+    persistentConnection: true,
   );
 
-  dio.httpClientAdapter = Http2Adapter(
-    ConnectionManager(
-      idleTimeout: const Duration(minutes: 1),
-      onClientCreate: (_, config) => config.onBadCertificate = (_) => true,
-    ),
-  );
+  (dio.httpClientAdapter as IOHttpClientAdapter).createHttpClient = () {
+    final client = HttpClient();
+    client.badCertificateCallback = (cert, host, port) => false;
+    return client;
+  };
 
   dio.interceptors.add(DioExceptionHandler());
 
