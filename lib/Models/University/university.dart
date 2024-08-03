576dbaf40951b669ee2b9aa6d2bcc8dfe1dab7ec
ibramaswadeh <ibrahim.masswadeh@gmail.com>
2024-08-03 21:19:35 +0300

    schema improvements

--- lib/Models/University/university.dart
+++ lib/Models/University/university.dart
@@ -6,24 +6,36 @@ import 'school.dart';
 class University {
   final String name;
   final String description;
+  final String location;
+  final String city;
+  final String website;
   final List<String> facts;
   final List<School> schools;
 
   const University(
       {required this.name,
       required this.description,
+      required this.location,
+      required this.city,
+      required this.website,
       required this.facts,
       required this.schools});
 
   University copyWith({
     String? name,
     String? description,
+    String? location,
+    String? city,
+    String? website,
     List<String>? facts,
     List<School>? schools,
   }) {
     return University(
       name: name ?? this.name,
       description: description ?? this.description,
+      location: location ?? this.location,
+      city: city ?? this.city,
+      website: website ?? this.website,
       facts: facts ?? this.facts,
       schools: schools ?? this.schools,
     );
@@ -37,6 +49,9 @@ class University {
     return University(
       name: map['name'] ?? '',
       description: map['description'] ?? '',
+      location: map['location'] ?? '',
+      city: map['city'] ?? '',
+      website: map['website'] ?? '',
       facts: List<String>.from(map['facts'] ?? []),
       schools: schools,
     );
--- lib/Models/User/userProps.dart
+++ lib/Models/User/userProps.dart
@@ -3,12 +3,14 @@ import 'package:flutter/foundation.dart';
 @immutable
 class UserProps {
   final String university;
+  final String school;
   final String major;
   final String contact;
   final String image;
 
   const UserProps({
     this.university = '',
+    this.school = '',
     this.major = '',
     this.contact = '',
     this.image = '',
@@ -16,12 +18,14 @@ class UserProps {
 
   UserProps copyWith({
     String? university,
+    String? school,
     String? major,
     String? contact,
     String? image,
   }) {
     return UserProps(
       university: university ?? this.university,
+      school: school ?? this.school,
       major: major ?? this.major,
       contact: contact ?? this.contact,
       image: image ?? this.image,
@@ -31,6 +35,7 @@ class UserProps {
   static UserProps fromMap(Map<String, dynamic> map) {
     return UserProps(
       university: map['university'] ?? '',
+      school: map['school'] ?? '',
       major: map['major'] ?? '',
       contact: map['contact'] ?? '',
       image: map['image'] ?? '',
